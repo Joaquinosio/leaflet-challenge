5d5af12859f16ad064505f2502bb09297a6c856f
Joaquin Osio <joaquinosio1@gmail.com>
2021-02-04 19:00:10 -0600

    We can now see the map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,45 +1,46 @@
-let mymap = L.map('map').setView([37.77, -122.41], 13);
+// Creating map object
+var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 8
+});
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
+    maxZoom: 18,
     zoomOffset: -1,
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
-}).addTo(mymap);
+}).addTo(myMap);
 
-let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// Load in GeoJson data
+var geoData = "static/data/all_week.geojson";
+var geojson;
 
-d3.json(url)
-.then(data=>{
-    
-    // data.forEach(d=>{
-        //     L.marker([d.location.coordinates[1], d.location.coordinates[0]]).addTo(mymap) 
-        // })
-        
-    // let markers = []
+// Grab data with d3
+d3.json(geoData, function(data) {
 
-    // data.forEach(d=>{
-    //     markers.push(L.marker([d.location.coordinates[1], d.location.coordinates[0]]))        
-    // })
+  // Create a new choropleth layer
+  geojson = L.HeatLayer(data, {
 
-    // L.layerGroup(markers).addTo(mymap)
+    // Define what  property in the features to use
+    valueProperty: "rms",
 
-    let heatArray = []
+    // Set color scale
+    scale: ["#ffffb2", "#b10026"],
 
-    data.forEach(d=>{
-        heatArray.push(
-            [d.location.mag[1], d.location.place[0]]
-        )
-    })
+    // Number of breaks in step range
+    steps: 10,
 
-    L.heatLayer(heatArray, {
-        radius: 20,
-        blur: 35
-    }).addTo(mymap)    
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+  }).addTo(myMap);
 
-})
-.catch(e=>{
-    console.log(e)
-})
+});
